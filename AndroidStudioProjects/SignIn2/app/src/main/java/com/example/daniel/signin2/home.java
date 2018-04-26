f8243ff6121c3a2c21f30f32ae08521a1d374c4e
Daniel <rodriguezdl@vcu.edu>
2018-04-26 16:07:56 -0400

    first commit

---
+++ AndroidStudioProjects/SignIn2/app/src/main/java/com/example/daniel/signin2/home.java
@@ -0,0 +1,53 @@
+package com.example.daniel.signin2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+
+public class home extends AppCompatActivity {
+    private CardView catalogCard;
+    private CardView chatCard;
+    private CardView upImgCard;
+    private CardView calCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        catalogCard=(CardView)findViewById(R.id.catalog);
+        chatCard=(CardView)findViewById(R.id.chatBoard);
+        upImgCard=(CardView)findViewById(R.id.uploadImg);
+        calCard=(CardView)findViewById(R.id.calendar);
+
+         catalogCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),catalog.class));
+            }
+        });
+
+        chatCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),catalog.class));
+            }
+        });
+
+        upImgCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),catalog.class));
+            }
+        });
+
+        calCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),catalog.class));
+            }
+        });
+    }
+}
+
