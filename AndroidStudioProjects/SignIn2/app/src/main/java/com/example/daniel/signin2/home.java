0b2f029693a94fab0e0bbcede90251a45b569a2e
Daniel <rodriguezdl@vcu.edu>
2018-04-26 16:14:38 -0400

    daniels first commit

--- AndroidStudioProjects/SignIn2/app/src/main/java/com/example/daniel/signin2/home.java
+++ AndroidStudioProjects/SignIn2/app/src/main/java/com/example/daniel/signin2/home.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
 
+
+// daniel ws here
 public class home extends AppCompatActivity {
     private CardView catalogCard;
     private CardView chatCard;
